167f92dfedbcfad6cf693696275c71c0d163b333
Khalil Panahi <khalil.panahi@accenture.com>
2021-11-05 08:19:13 -0300

    Add a controller "ListController" for new screen

---
+++ GetItDone/ListController.swift
@@ -0,0 +1,14 @@
+//
+//  ListController.swift
+//  GetItDone
+//
+//  Created by khalil.panahi on 05/11/21.
+//
+
+import UIKit
+
+class ListController: UIViewController{
+    override func viewDidLoad() {
+        view.backgroundColor = .purple
+    }
+}
